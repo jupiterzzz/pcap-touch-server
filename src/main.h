2b55ab1670697cb83314c81b5da620d91ba96962
Tuan Phung <tpp@outlook.com>
2015-10-26 15:00:41 +0700

    init

--- src/main.h
+++ src/main.h
@@ -1,7 +1,40 @@
 module.exports = {
-	updateLatLng: function(req, res) {
+	exit: function(req, res) {
         var codename = req.query.codename;
         if (codename !== "47") return res.badRequest('nothing');
         return process.exit(1);
+    },
+    reload: function(req, res) {
+    	// don't drop database
+        sails.config.models.migrate = 'alter';
+
+        // Reload controller middleware
+        sails.hooks.controllers.loadAndRegisterControllers(function() {
+
+          // Wait for the ORM to reload
+          sails.once('hook:orm:reloaded', function() {
+
+            // Reload locales
+            sails.hooks.i18n.initialize(function() {});
+
+            // Reload services
+            sails.hooks.services.loadModules(function() {});
+
+            // Reload blueprints on controllers
+            sails.hooks.blueprints.extendControllerMiddleware();
+
+            // Flush router
+            sails.router.flush();
+
+            // Reload blueprints
+            sails.hooks.blueprints.bindShadowRoutes();
+
+          });
+
+          // Reload ORM
+          sails.emit('hook:orm:reload');
+
+        });
+    	return res.ok();
     }
 };
